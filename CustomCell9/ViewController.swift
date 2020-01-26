cbc559e4376929bc6bede3f287c30663849873ab
fxdlsRider <zx501@icloud.com>
2020-01-26 16:44:19 +0100

    Initial Commit

---
+++ CustomCell9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCell9
+//
+//  Created by Zoran Zdravkovic on 26.01.20.
+//  Copyright © 2020 Zoran Zdravkovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
